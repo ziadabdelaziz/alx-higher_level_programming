bafe64b627f6b0caf328bfd2eb355fc690a759f6
ziad <ziadabdelaziz1995@gmail.com>
2023-05-11 01:33:18 +0300

    [Update]: two pointer check cyle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * check_cycle - check if there is 
+ * a cycle in linked list
+ * @list: linked list
+ * Return: 0 if no cycle or 1 otherwise
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *prev, *cur;
+	
+	if (list == NULL || list->next == NULL)
+		return (0);
+
+	prev = NULL;
+	cur = list;
+
+	while (cur->next)
+	{
+		prev = cur;
+		cur = cur->next;
+		if (prev->next != cur)
+			return (1);
+	}
+
+	return (0);
+}
