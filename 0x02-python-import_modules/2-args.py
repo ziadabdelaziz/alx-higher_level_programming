52af80edc09fd6a639e74b4b6daf7ff5af9f64c4
ziad <ziadabdelaziz1995@gmail.com>
2023-05-11 20:54:11 +0300

    [FIX]: removing exit + style fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-if __name__ == "__main__":    
+if __name__ == "__main__":
     from sys import argv
 
     len = len(argv)
 
     if len == 1:
         print("{:d} arguments.".format(len - 1))
-        exit()
     elif len == 2:
         print("{:d} argument:".format(len - 1))
     else:
         print("{:d} argument:".format(len - 1))
-    
-    for i in range(1,len):
-        print("{:d}: {:s}".format(i, argv[i]))
+
+    if len >= 2:
+        for i in range(1, len):
+            print("{:d}: {:s}".format(i, argv[i]))
