c5f1893b6fdf69a73fafcfec56ed85548cb4cd6a
ziad <ziadabdelaziz1995@gmail.com>
2023-05-13 01:33:39 +0300

    [FIX]: code style

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     import importlib
     module = importlib.import_module("hidden_4")
     names = dir(module)
-    
+
     for name in sorted(names):
         if name.startswith("__"):
             continue
