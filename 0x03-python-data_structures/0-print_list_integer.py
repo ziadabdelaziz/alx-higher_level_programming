8d83de1c195fb28dd2557b95ce55aff7cf76cfcf
ziad <ziadabdelaziz1995@gmail.com>
2023-05-14 19:47:41 +0300

    [FIX]: using str.format + style

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for num in my_list:
-        print(str.format(num))
+        print("{:d}".format(num))
+
+
+print_list_integer([2, 5, 10, -5])
