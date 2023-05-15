d38138ec479c30dd6df293190b4c418632e3e642
ziad <ziadabdelaziz1995@gmail.com>
2023-05-15 16:56:07 +0300

    [FIX]: edit conditions

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,10 +3,6 @@ def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
             print("{:d}".format(matrix[i][j]), end='')
-            if j == (len(matrix[0]) - 1):
-                print(end='$\n')
-            else:
+            if j != (len(matrix[0]) - 1):
                 print(end=' ')
-
-    if len(matrix[0]) == 0:
-        print("$")
+        print(end='$\n')
