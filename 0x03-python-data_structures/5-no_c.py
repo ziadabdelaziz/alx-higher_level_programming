d934082de4684762abcf90a216e259f6bd213b42
ziad <ziadabdelaziz1995@gmail.com>
2023-05-15 15:46:20 +0300

    [Update]: string editing

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+def no_c(my_string):
+    new_string = ''
+    length = len(my_string)
+    cur, prev = 0, 0
+
+    while cur < length:
+        if my_string[cur] in ['c', 'C']:
+            new_string += my_string[prev:cur]
+            prev = cur + 1
+        cur += 1
+
+    if prev < cur:
+        new_string += my_string[prev:cur]
+
+    return new_string
