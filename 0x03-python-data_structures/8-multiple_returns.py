a9f58c971e28ea84f656e265b3dc91e2567412b6
ziad <ziadabdelaziz1995@gmail.com>
2023-05-16 18:39:30 +0300

    [FIX]: empty string first char is None

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,6 +3,6 @@ def multiple_returns(sentence):
     if sentence is None:
         return 0, None
     if sentence == '':
-        return 0, ''
+        return 0, None
 
     return len(sentence), sentence[0]
