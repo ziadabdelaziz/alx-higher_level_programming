2c3de0f979d62919f0ddf3eabd47e4a94d4b73eb
ziad <ziadabdelaziz1995@gmail.com>
2023-05-16 03:56:45 +0300

    [Update]: add tuples

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    if isinstance(tuple_a, int):
+        tuple_a = (tuple_a, 0)
+    elif len(tuple_a) == 0:
+        tuple_a(0, 0)
+    else:
+        tuple_a = (tuple_a[0], tuple_a[1])
+
+    if isinstance(tuple_b, int):
+        tuple_b = (tuple_b, 0)
+    elif len(tuple_b) == 0:
+        tuple_b(0, 0)
+    else:
+        tuple_b = (tuple_b[0], tuple_b[1])
+
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
