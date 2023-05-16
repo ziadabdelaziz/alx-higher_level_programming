db4c8b1b8b7a431a9d7a92c67feb75409e81e212
ziad <ziadabdelaziz1995@gmail.com>
2023-05-16 18:27:02 +0300

    [FIX]: add tupe - refactor + handle cases

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if isinstance(tuple_a, int):
-        tuple_a = (tuple_a, 0)
-    elif len(tuple_a) == 0:
-        tuple_a(0, 0)
-    else:
-        tuple_a = (tuple_a[0], tuple_a[1])
+    first = 1
+    a, b = tuple_a, tuple_b
+    for tup in [tuple_a, tuple_b]:
+        if isinstance(tup, int):
+            tup = (tup, 0)
+        elif len(tup) == 0:
+            tup = (0, 0)
+        elif len(tup) == 1:
+            tup = (tup[0], 0)
+        else:
+            tup = (tup[0], tup[1])
 
-    if isinstance(tuple_b, int):
-        tuple_b = (tuple_b, 0)
-    elif len(tuple_b) == 0:
-        tuple_b(0, 0)
-    else:
-        tuple_b = (tuple_b[0], tuple_b[1])
+        if first:
+            a = tup
+            first = 0
+        else:
+            b = tup
 
-    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return (a[0] + b[0], a[1] + b[1])
