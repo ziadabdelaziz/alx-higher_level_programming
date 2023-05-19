7d1f1c6de3aecae02706c2af0e11e679fd9fb5c5
ziad <ziadabdelaziz1995@gmail.com>
2023-05-19 21:16:31 +0300

    [FIX]:

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,7 +3,7 @@ def square_matrix_simple(matrix=[]):
     if matrix is None:
         return matrix
 
-    square_matrix = matrix.copy()
+    square_matrix = [[0] * len(matrix[0]) for _ in range(len(matrix))]
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
             square_matrix[i][j] = matrix[i][j] * matrix[i][j]
