a1c3f1493eb5998a2ff24706640e8e8b0173efd8
ziad <ziadabdelaziz1995@gmail.com>
2023-05-30 04:10:15 +0300

    [ADD]: task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    cnt = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end='')
+            cnt += 1
+    except:
+        print()
+        return cnt
+    
+    print()
+    return cnt
