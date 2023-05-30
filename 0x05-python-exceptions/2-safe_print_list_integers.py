5cabab9aac2abebf08fdbbba5ef98b3442043565
ziad <ziadabdelaziz1995@gmail.com>
2023-05-30 05:02:23 +0300

    [ADD]: task 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+def safe_print_list_integers(my_list=[], x=0):
+    cnt = 0
+    for i in range(x):
+        elm = my_list[i]
+        try:
+            print("{:d}".format(elm), end='')
+            cnt += 1
+        except Exception as e:
+            continue
+
+    print()
+    return cnt
