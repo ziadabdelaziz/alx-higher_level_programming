f2b5a3264f584a2aa2d50bb353ba0304386452bd
ziad <ziadabdelaziz1995@gmail.com>
2023-05-30 20:42:23 +0300

    [ADD]: safe instantiation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -13,9 +13,14 @@ class Square:
 
     __size = 0
 
-    def __init__(self, size):
+    def __init__(self, size=0):
         """
-        Instantiate 'size' attribute
+        Instantiate 'size' attribute safely
         """
 
-        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
