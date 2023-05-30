87e69c3b0ee23ee99aff4701efebb12b60405608
ziad <ziadabdelaziz1995@gmail.com>
2023-05-30 20:34:14 +0300

    [ADD]: private attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This is module-level documentation
+In this module we define a Square
+"""
+
+
+class Square:
+    """
+    This is a'Square'
+    Has a private attribute 'size'
+    """
+
+    __size = 0
+
+    def __init__(self, size):
+        """
+        Instantiate 'size' attribute
+        """
+
+        self.__size = size
