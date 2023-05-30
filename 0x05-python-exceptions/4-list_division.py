6d1e7d3183e9bcf4abec822d6ebc041ed8172033
ziad <ziadabdelaziz1995@gmail.com>
2023-05-30 16:14:28 +0300

    [ADD]: multiple excepts

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = []
+
+    for i in range(list_length):
+        value = 0
+
+        try:
+            value = my_list_1[i] / my_list_2[i]
+        except TypeError:
+            print("wrong Type")
+        except ZeroDivisionError:
+            print("divsion by 0")
+        except IndexError:
+            print("out of range")
+        finally:
+            new_list.append(value)
+
+    return new_list
