41b09a561cd9248eb16b38e3ccb5635ec9bc1693
ziad <ziadabdelaziz1995@gmail.com>
2023-06-13 01:17:25 +0300

    [ADD]: is class or parent class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+this module includes
+is_kind_of_class method
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    check if obj is an instance of a_class
+    or it's parent class
+    """
+    return type(obj) is a_class or isinstance(obj)
