cd3e215ef7065540ba6c0087adf3567eca790799
ziad <ziadabdelaziz1995@gmail.com>
2023-06-13 01:50:27 +0300

    [ADD]: Rectangle class

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+Includes Rectangle class
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    inherits BaseGeometry
+    """
+
+    def __init__(slef, width, height):
+        """
+        initialize width and height
+        """
+        pass
+
+    @property
+    def width(self):
+        """
+        width getter
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        width setter
+        """
+        self.integer_validator(value)
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+        height getter
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        height setter
+        """
+        self.integer_validator(value)
+        self.__height = value
