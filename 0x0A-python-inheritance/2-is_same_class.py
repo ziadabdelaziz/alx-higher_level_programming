f1d786639151e1a5db0a6ed9727326730e1d6a4d
ziad <ziadabdelaziz1995@gmail.com>
2023-06-13 01:08:55 +0300

    [ADD]: isinstance

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+this module contains
+is_same_class method
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    check if the object is an instance
+    of the specified class
+    """
+    return isinstance(obj, a_class)
