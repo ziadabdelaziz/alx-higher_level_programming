e017a80165f2451f04a0887a43327a6fca896fb5
ziad <ziadabdelaziz1995@gmail.com>
2023-06-13 01:32:58 +0300

    [ADD]: not implemented funciton

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Includes BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """
+    includes one unimplemented method
+    """
+
+    def area(self):
+        """
+        raise not implemented exception
+        """
+        raise Exception("area() is not implemented")
