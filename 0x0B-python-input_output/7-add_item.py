2997c4221205e09738afb96d66533a45f6f71257
ziad <ziadabdelaziz1995@gmail.com>
2023-06-13 18:55:55 +0300

    [ADD]: arguments to json

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+
+"""
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+arguments = sys.argv[1:]
+save_to_json_file(arguments, 'add_item.json')
+print(load_from_json_file('add_item.json'))
