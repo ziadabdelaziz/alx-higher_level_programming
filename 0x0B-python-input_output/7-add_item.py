bc9933c47fbcaa37d5c378dbfe5a5be443796930
ziad <ziadabdelaziz1995@gmail.com>
2023-06-13 18:58:05 +0300

    [ADD]: [FIX]: added documentation and removed print

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """
-
+get command line arguments list
+convert them json
+and save them to json file
 """
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
@@ -9,4 +11,3 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 arguments = sys.argv[1:]
 save_to_json_file(arguments, 'add_item.json')
-print(load_from_json_file('add_item.json'))
