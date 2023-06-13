fa97fd488e3b14689800c290c403dd34cb330eec
ziad <ziadabdelaziz1995@gmail.com>
2023-06-13 21:39:26 +0300

    [ADD]: student class

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Student class
+"""
+
+
+class Student:
+    """
+    Student with fname, lname and age
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        initialization function
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    
+    def to_json(self):
+        """
+        return dictionary representation
+        """
+        return {
+            "first_name":self.first_name,
+            "last_name": self.last_name,
+            "age": self.age,
+        }
